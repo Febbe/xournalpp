d1243f35ce97626a0f3cc43ca0cabd8d9c55d05f
Fabian Keßler <fabian_kessler@gmx.de>
2021-07-17 11:09:17 +0200

    Update src/model/Stroke.cpp
    Co-authored-by: Roland Lötscher <40485433+rolandlo@users.noreply.github.com>

--- src/model/Stroke.cpp
+++ src/model/Stroke.cpp
@@ -166,7 +166,7 @@ void Stroke::setLastPoint(const Point& p) {
 void Stroke::addPoint(const Point& p) {
     this->points.emplace_back(p);
     updateBounds(Element::x, Element::y, Element::width, Element::height, Element::snappedBounds, p,
-                 hasPressure() ? p.z / 2.0 : this->width);
+                 hasPressure() ? p.z / 2.0 : this->width / 2.0);
 }
 
 auto Stroke::getPointCount() const -> int { return this->points.size(); }
